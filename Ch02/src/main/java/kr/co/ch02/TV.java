fcee2f2b920697319001ccdf801f17935985473d
sorkcoavus <sorkcoavus@gmail.com>
2023-01-02 13:22:36 +0900

    1/2 스프링 첫날

---
+++ Ch02/src/main/java/kr/co/ch02/TV.java
@@ -0,0 +1,13 @@
+package kr.co.ch02;
+
+public interface TV {
+	public void powerOn();
+	public void powerOff();
+
+	public void chUp();
+	public void chDown();
+	
+	public void soundUp();
+	public void soundDown();
+	
+}
